e7385e975659cf76e423c0a869b549df0fcbf540
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-12-27 15:13:09 +0000

    Translated example into something like real ES6

---
+++ app.js
@@ -0,0 +1,41 @@
+class MainScene extends Phaser.Scene {
+  preload() {
+    this.load.setBaseURL("http://labs.phaser.io");
+    this.load.image("sky", "assets/skies/space3.png");
+    this.load.image("logo", "assets/sprites/phaser3-logo.png");
+    this.load.image("red", "assets/particles/red.png");
+  }
+
+  create() {
+    this.add.image(400, 300, "sky");
+
+    let particles = this.add.particles("red");
+
+    let emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: "ADD",
+    });
+
+    let logo = this.physics.add.image(400, 100, "logo");
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.startFollow(logo);
+  }
+}
+
+let game = new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 800, // window.innerWidth
+  height: 600, // window.innerHeight
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: MainScene,
+});
